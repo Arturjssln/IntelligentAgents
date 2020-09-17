fdcc84d1a5320144ee865bb11fc681e5f536869b
Arturjesslen <artur.jesslen@epfl.ch>
2020-09-17 13:09:55 +0200

    First part of project in progress

--- Exercise1-rabbits/270642-271518-in/src/RabbitsGrassSimulationModel.java
+++ Exercise1-rabbits/270642-271518-in/src/RabbitsGrassSimulationModel.java
@@ -1,6 +1,7 @@
 import uchicago.src.sim.engine.Schedule;
 import uchicago.src.sim.engine.SimModelImpl;
 import uchicago.src.sim.engine.SimInit;
+import uchicago.src.sim.gui.DisplaySurface;
 
 /**
  * Class that implements the simulation model for the rabbits grass
@@ -9,11 +10,35 @@ import uchicago.src.sim.engine.SimInit;
  * environment and the simulation.
  *
  * @author 
+ * - Célia Benquet - 271518
+ * - Artur Jesslen - 270642
  */
 
 
 public class RabbitsGrassSimulationModel extends SimModelImpl {		
-
+		
+		// Default values
+		private static final int GRIDSIZE = 20;
+		private static final int NUMINITRABBITS = 100; //TODO!!
+		private static final int NUMINITGRASS = 100; //TODO!!
+		private static final int GRASSGROWTHRATE = 100; //TODO!!
+		private static final int BIRTHTHRESHOLD = 100; //TODO!!
+	
+		// Variables
+		private Schedule schedule;
+		private RabbitsGrassSimulationSpace rgSpace;
+		private DisplaySurface displayEcosystem;
+		
+		// Attributes
+		private int gridSize = GRIDSIZE;
+		private int numInitRabbits = NUMINITRABBITS;
+		private int numInitGrass = NUMINITGRASS;
+		private int grassGrowthRate = GRASSGROWTHRATE;
+		private int birthThreshold = BIRTHTHRESHOLD;
+		
+		
+	
+		// Methods 
 		public static void main(String[] args) {
 			
 			System.out.println("Rabbit skeleton");
@@ -27,9 +52,36 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 				init.loadModel(model, args[0], Boolean.parseBoolean(args[1]));
 			
 		}
+		
+		public void setup() {
+			rgSpace = null;
+			
+			if (displayEcosystem != null) {
+				displayEcosystem.dispose();
+				displayEcosystem = null;
+		    }
+			displayEcosystem = new DisplaySurface(this, "Carry Drop Model Window 1");
+			registerDisplaySurface("Carry Drop Model Window 1", displayEcosystem);
+		}
 
 		public void begin() {
+			buildModel();
+			buildSchedule();
+			buildDisplay();
 			
+		}
+		
+		public void buildModel() {
+			rgSpace = new RabbitsGrassSimulationSpace(gridSize);
+			rgSpace.initializeGrass(numInitGrass);
+		}
+		
+		public void buildSchedule() {
+
+		}
+		
+		public void buildDisplay() {
+
 		}
 
 		public String[] getInitParam() {
@@ -44,15 +96,50 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 			// TODO Auto-generated method stub
 			return "Rabbits simulation";
 		}
-
+		
 		public Schedule getSchedule() {
 			// TODO Auto-generated method stub
-			return null;
+			return schedule;
 		}
 
-		public void setup() {
-			// TODO Auto-generated method stub
-			System.out.println(this.);
-			
+		public int getGridSize() {
+			return gridSize;
+		}
+		
+		public int getNumInitRabbits() {
+			return numInitRabbits;
+		}
+		
+		public int getNumInitGrass() {
+			return numInitGrass;
+		}
+		
+		public int getGrassGrowthRate() {
+			return grassGrowthRate;
+		}
+		
+		public int getBirthThreshold() {
+			return birthThreshold;
+		}
+		
+		public void setGridSize(int n) {
+			gridSize = n;
+		}
+		
+		public void setNumInitRabbits(int n) {
+			numInitRabbits = n;
+		}
+		
+		
+		public void setNumInitGrass(int n) {
+			numInitGrass = n;
+		}
+		
+		public void setGrassGrowthRate(int n) {
+			grassGrowthRate = n;
+		}
+		
+		public void setBirthThreshold(int n) {
+			birthThreshold = n;
 		}
 }
--- Exercise1-rabbits/270642-271518-in/src/RabbitsGrassSimulationSpace.java
+++ Exercise1-rabbits/270642-271518-in/src/RabbitsGrassSimulationSpace.java
@@ -1,8 +1,36 @@
+import uchicago.src.sim.space.Object2DGrid;
+
 /**
  * Class that implements the simulation space of the rabbits grass simulation.
  * @author 
  */
 
 public class RabbitsGrassSimulationSpace {
-
+	private Object2DGrid ecosystem;
+	
+	public RabbitsGrassSimulationSpace(int size) {
+		ecosystem = new Object2DGrid(size, size);
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				ecosystem.putObjectAt(i, j, 0);
+			}
+		}
+	}
+	
+	public void initializeGrass(int n) {
+	    for(int i = 0; i < n; i++){
+	    	int x, y;
+	    	// Look for an empty cell
+	    	do {
+	    		x = (int)(Math.random()*(ecosystem.getSizeX()));
+	    		y = (int)(Math.random()*(ecosystem.getSizeY()));
+	    	} while((Integer)ecosystem.getObjectAt(x,y) != 0);
+	    	// Fill the empty cell with a grass object
+	    	ecosystem.putObjectAt(x, y, 1);
+	    }
+	}
+	
+	public int getGrassQuantityAt(int x, int y) {
+		return (Integer)ecosystem.getObjectAt(x,y);
+	}
 }
