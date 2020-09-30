18cdd2dde8b4f7b1acec7a958ee6c0fdcdae4222
Arturjesslen <artur.jesslen@epfl.ch>
2020-09-30 16:43:41 +0200

    learnValue implemented

--- Exercise2-reactiveAgent/reactive/src/template/ReactiveTemplate.java
+++ Exercise2-reactiveAgent/reactive/src/template/ReactiveTemplate.java
@@ -1,7 +1,10 @@
 package template;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 
+import apple.laf.JRSUIState.ValueState;
 import logist.simulation.Vehicle;
 import logist.agent.Agent;
 import logist.behavior.ReactiveBehavior;
@@ -15,10 +18,18 @@ import logist.topology.Topology.City;
 
 public class ReactiveTemplate implements ReactiveBehavior {
 
+	static private final double EPSILON = 10e-4;
+
 	private Random random;
 	private double pPickup;
 	private int numActions;
 	private Agent myAgent;
+	private double[] values;
+	
+
+	private HashMap<City, Double> bestValueInState = new HashMap<City, Double>();
+	private HashMap<City, City> bestStateInState = new HashMap<City, City>();
+
 
 	@Override
 	public void setup(Topology topology, TaskDistribution td, Agent agent) {
@@ -32,12 +43,15 @@ public class ReactiveTemplate implements ReactiveBehavior {
 		this.pPickup = discount;
 		this.numActions = 0;
 		this.myAgent = agent;
+		
+		learnValue(topology, td);
+
 	}
 
 	@Override
 	public Action act(Vehicle vehicle, Task availableTask) {
 		Action action;
-
+		
 		if (availableTask == null || random.nextDouble() > pPickup) {
 			City currentCity = vehicle.getCurrentCity();
 			action = new Move(currentCity.randomNeighbor(random));
@@ -51,5 +65,47 @@ public class ReactiveTemplate implements ReactiveBehavior {
 		numActions++;
 		
 		return action;
+	}
+
+	
+	private void learnValue(Topology topology, TaskDistribution td) {
+		double maxDiff;
+		double maxQValue;
+		int nbCities = topology.size();
+		double[] oldValues = new double[nbCities];
+		double[] qValue = new double[nbCities];
+		double[] values = new double[nbCities];
+		for (double val : values) {
+			val = (new Random()).nextInt(10);
+		}
+		do {
+			maxDiff = Double.MIN_VALUE;
+			oldValues = values;
+			for (int s=0; s<nbCities; s++) {
+				maxQValue = Double.MIN_VALUE;
+				Arrays.fill(qValue, Double.MIN_VALUE);
+				City from = topology.cities().get(s);
+				for (int a=0; a<nbCities; a++) {
+					double sumProba = 0;
+					City to = topology.cities().get(a);
+					if (s==a) {continue;}
+					for (int i=0; i<nbCities; i++) {
+						sumProba += td.probability(from, to) * oldValues[i];
+					}
+					qValue[a] = td.reward(from, to) + this.pPickup * sumProba;
+				}
+				City bestCity = from;
+				for(int i=0; i<nbCities; i++) {
+					maxQValue = qValue[i] > maxQValue ? qValue[i] : maxQValue;
+					bestCity = qValue[i] > maxQValue ? topology.cities().get(i) : bestCity;
+				}
+				this.bestValueInState.put(from, maxQValue);
+				this.bestStateInState.put(from, bestCity);
+			}
+			for(int i=0; i<nbCities; i++) {
+				double diff = Math.abs(oldValues[i] - values[i]);
+				maxDiff = diff > maxDiff ? diff : maxDiff;
+			}
+		} while(maxDiff > EPSILON);
 	}
 }
